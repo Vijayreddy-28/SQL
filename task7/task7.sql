bb2f152f30e55b7b496a0be3e55a1bdc33952f62
Vijayreddy-28 <vijayreddy00123@gmail.com>
2026-03-18 23:46:08 +0530

    Improve SQL queries in task7.sql
    Refactor SQL queries for better readability and consistency.

--- task7/task7.sql
+++ task7/task7.sql
@@ -6,7 +6,7 @@ select * from Employee;
 
 -- Find the second highest Salary for each department
 SELECT 
-	   department,salary 
+	department,salary 
     FROM (
     SELECT department,salary,
     DENSE_RANK() OVER(PARTITION BY department ORDER BY salary DESC) as rnk 
@@ -15,24 +15,24 @@ SELECT
     
 -- Find the top 3 highest-paid employees in the company
 SELECT 
-	   T.employee_name, T.salary
-	   FROM (
+	T.employee_name, T.salary
+	FROM (
     SELECT CONCAT(first_name, ' ', last_name) AS employee_name,
     salary,
-	   RANK() OVER (ORDER BY salary DESC) AS rnk
+	RANK() OVER (ORDER BY salary DESC) AS rnk
     FROM Employee) T
     WHERE rnk <= 3;
     
 -- Show each employee’s salary and the total salary of their department.
 SELECT 
-	   first_name,salary,
+	first_name,salary,
     SUM(salary) OVER(PARTITION BY department) as total_salary_dept 
     from Employee 
     ORDER BY total_salary_dept DESC;
 
 -- Identify employees whose salary is greater than the previous employee’s salary.
 SELECT 
-	   T.emp_id,T.emp_name,T.salary
+	T.emp_id,T.emp_name,T.salary
     FROM (
     SELECT CONCAT(first_name," ",last_name) as emp_name,
     emp_id,salary,
